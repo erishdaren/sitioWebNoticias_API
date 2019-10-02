c9840a0741c47a0814a830b404bd31f38593e7e4
Erick Moreno <moreno240190@gmail.com>
2019-10-02 11:31:49 -0500

    Se ingreso el API al State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Header from './components/Header';
-function App() {
-  return (
-    <div className="App">
-        <Header titulo = "Sitio Web de Noticias" />
-    </div>
-  );
+
+export default class Class extends Component {
+state = {
+  noticias : []
 }
 
-export default App;
+
+
+  componentDidMount() {
+    this.consultarNoticias();
+  }
+  consultarNoticias = () => {
+      //const url = `https://newsapi.org/v2/top-headlines? country = mx & apiKey = 65b0783607704b2ca764074365487cb9`;
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=general&apiKey=65b0783607704b2ca764074365487cb9`;
+      fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then(noticias => {
+        this.setState({
+          noticias : noticias.articles
+        })
+      })
+  }
+    render() {
+        return (
+            <div className="class-name">
+                 <Header titulo = "Sitio Web de Noticias" />
+            </div>
+        );
+    }
+}
+
+
+
